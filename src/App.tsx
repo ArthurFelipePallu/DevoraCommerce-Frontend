4034f2701cc75d339bd33c81e54683475d8f438c
Arthur Felipe Pallu <arthurfelipepallu@gmail.com>
2024-08-04 16:59:18 -0300

    Link to inexisting pag

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Catalog from "./routes/Home/Catalog";
 import ProductDetails from "./routes/Home/ProductDetails";
@@ -14,6 +14,7 @@ export default function App() {
         <Route path="catalog" element={<Catalog />} />
         <Route path="product-details/:productId" element={<ProductDetails />} />
       </Route>
+      <Route path="*" element={ <Navigate to="/" /> } />
     </Routes>
   </BrowserRouter>
   );
