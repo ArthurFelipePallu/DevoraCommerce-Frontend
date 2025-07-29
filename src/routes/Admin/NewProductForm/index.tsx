2559caacb6b7e63b85b7b077ae939029490bc7be
Arthur Felipe Pallu <arthurfelipepallu@gmail.com>
2025-07-29 09:53:04 -0300

    dirty and validate all

--- src/routes/Admin/NewProductForm/index.tsx
+++ src/routes/Admin/NewProductForm/index.tsx
@@ -1,7 +1,7 @@
-import "./styles.css"
+import "./styles.css";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import  * as forms from "../../../utils/forms";
+import * as forms from "../../../utils/forms";
 import { history } from "../../../utils/history";
 import FormInput from "../../../Components/FormInput";
 import * as selectStyles from "../../../utils/select";
@@ -14,223 +14,215 @@ import * as categoryService from "../../../services/category-service";
 import ActionBlueButton from "../../../Components/Buttons/ActionBlueButton";
 import ActionWhiteButton from "../../../Components/Buttons/ActionWhiteButton";
 
+export default function NewProductForm() {
+  const params = useParams();
 
-export default function NewProductForm()
-{
+  const isEditing = params.productId !== "create";
 
-    const params = useParams();
+  const [categories, setCategories] = useState<CategoryDTO[]>([]);
 
-    const isEditing = params.productId !== 'create'; 
+  const [formData, setFormData] = useState<any>({
+    name: {
+      value: "",
+      id: "name",
+      name: "name",
+      type: "text",
+      placeholder: "Product Name",
+      validation: function (value: string) {
+        const regex = /^[a-zA-Z0-9\s\.,_-]+$/; //expresão regular REGEX - específica uma maneiraque o texto deve ser escrito, como formatos de telefones, email , etc
+        return regex.test(value);
+      },
+      message:
+        "Favor informar um nome válido com primeiras letras maíusculas e sem números",
+    },
+    price: {
+      value: "",
+      id: "price",
+      name: "price",
+      type: "number",
+      placeholder: "Product Price",
+      validation: function (value: any) {
+        return Number(value) > 0;
+      },
+      message: "Favor informar um valor positivo",
+    },
+    imgUrl: {
+      value: "",
+      id: "imgUrl",
+      name: "imgUrl",
+      type: "text",
+      placeholder: "Imagem",
+      message: "Favor carregar uma imagem válida",
+    },
+    categories: {
+      value: [],
+      id: "categories",
+      name: "categories",
+      type: "text",
+      placeholder: "Categorias",
+      validation: function (value: CategoryDTO[]) {
+        return value.length > 0;
+      },
+      message: "Pelo menos uma categoria deve ser selecionada",
+    },
+    description: {
+      value: "",
+      id: "description",
+      name: "description",
+      type: "text",
+      placeholder: "Descrição do produto",
+      validation: function (value: string) {
+        //expresão regular REGEX com minimo 10 caracteres e sem máximo
+        return /^.{10,}$/.test(value);
+      },
+      message: "Descrição deve ter pelo menos 10 caracteres",
+    },
+  });
 
-    const [categories,setCategories] = useState<CategoryDTO[]>([]);
+  useEffect(() => {
+    categoryService
+      .findAllRequest()
+      .then((response) => {
+        setCategories(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    const [formData, setFormData] = useState<any>({
-                                              name:{
-                                                value: "",
-                                                id:"name",
-                                                name:"name",
-                                                type:"text",
-                                                placeholder:"Product Name",
-                                                validation: function(value:string){
-                                                  const regex = /^[a-zA-Z0-9\s\.,_-]+$/;//expresão regular REGEX - específica uma maneiraque o texto deve ser escrito, como formatos de telefones, email , etc
-                                                  return regex.test(value); 
-                                                },
-                                                message:"Favor informar um nome válido com primeiras letras maíusculas e sem números"
-                                              },
-                                              price:{
-                                                value: "",
-                                                id:"price",
-                                                name:"price",
-                                                type:"number",
-                                                placeholder:"Product Price",                                                
-                                                validation: function(value:any){
-                                                  return Number(value) > 0;
-                                                },
-                                                message:"Favor informar um valor positivo",
-                                              },
-                                              imgUrl:{
-                                                value: "",
-                                                id:"imgUrl",
-                                                name:"imgUrl",
-                                                type:"text",
-                                                placeholder:"Imagem",
-                                                message:"Favor carregar uma imagem válida"
-                                              },
-                                              categories:{
-                                                value : [],
-                                                id:"categories",
-                                                name:"categories",
-                                                type:"text",
-                                                placeholder:"Categorias",
-                                                validation: function(value: CategoryDTO[]){
-                                                  return value.length > 0;
-                                                },
-                                                message:"Pelo menos uma categoria deve ser selecionada"
-                                              },
-                                              description:{
-                                                value: "",
-                                                id:"description",
-                                                name:"description",
-                                                type:"text",
-                                                placeholder:"Descrição do produto",
-                                                validation: function(value:string){
-                                                    //expresão regular REGEX com minimo 10 caracteres e sem máximo
-                                                  return /^.{10,}$/.test(value); 
-                                                },
-                                                message:"Descrição deve ter pelo menos 10 caracteres"
-                                              }
-                                            });
-
-                                            
-    useEffect ( () =>{
-        categoryService.findAllRequest()
-            .then(response => {
-                setCategories(response.data);
-            }).catch(error => { console.log(error)})
-    } , []);                                        
-                   
-    useEffect( () => {
-        if(isEditing)
-        {
-            const id = Number(params.productId);
-            productService.findById(id).then( response => {
-                const newFormData = forms.updateAll(formData,response.data);
-                setFormData(newFormData);
-            } );
-        }
-    } , []);
-
-    function handleSubmit()
-    {
-
-        console.log(forms.toValues(formData));
+  useEffect(() => {
+    if (isEditing) {
+      const id = Number(params.productId);
+      productService.findById(id).then((response) => {
+        const newFormData = forms.updateAll(formData, response.data);
+        setFormData(newFormData);
+      });
     }
+  }, []);
 
-    function CancelForm()
-    {
-        returnTo("/admin/products");
+  function handleSubmit() {
+    const formValidated = forms.dirtyAndValidateAll(formData);
+    
+    if (forms.hasAnyInvalid(formValidated)) {
+      setFormData(formValidated);
+      return;
     }
-    function SaveForm()
-    {
-        returnTo("/admin/products");
-    }
-    function returnTo(path:string)
-    {
-        history.push(path);
-    }
+  }
 
-    const CancelButton:ActionButtonDTO ={
-        id:1,
-        name:"Cancelar",
-        action:CancelForm
-    }
-    const SaveButton:ActionButtonDTO ={
-        id:2,
-        name:"Salvar",
-        action:handleSubmit
-    }
+  function CancelForm() {
+    returnTo("/admin/products");
+  }
+  function SaveForm() {}
 
-    function imageSelected(event : any)
-    {
-        forms.update(formData,event.target.name,event.target.files[0]);
-    }
-    function updateForm(event : any)
-    {
-        //forms atualizado e validado
-        const result = forms.updateAndValidate(formData,event.target.name,event.target.value);
-        //forms setado para valores novos
-        setFormData(result);
-    }
-    function turnInputDirty(name : string)
-    {
-        setFormData(forms.dirtyAndValidate(formData,name));
-    }
+  function returnTo(path: string) {
+    history.push(path);
+  }
 
+  const CancelButton: ActionButtonDTO = {
+    id: 1,
+    name: "Cancelar",
+    action: CancelForm,
+  };
+  const SaveButton: ActionButtonDTO = {
+    id: 2,
+    name: "Salvar",
+    action: handleSubmit,
+  };
 
+  function imageSelected(event: any) {
+    forms.update(formData, event.target.name, event.target.files[0]);
+  }
+  function updateForm(event: any) {
+    //forms atualizado e validado
+    const result = forms.updateAndValidate(
+      formData,
+      event.target.name,
+      event.target.value
+    );
+    //forms setado para valores novos
+    setFormData(result);
+  }
+  function turnInputDirty(name: string) {
+    setFormData(forms.dirtyAndValidate(formData, name));
+  }
 
-    return(
-        <>
-            <section className="devcom-new-product-form-section ">
-                <div className="devcom-container devcom-card devcom-container-column-center">
-                    <h1>DADOS DO PRODUTO</h1>
-                    <form className="devcom-container-column-center devcom-new-product-form">
-                        <div>
-                            <FormInput 
-                                {...formData.name}
-                                onChange={updateForm}
-                                className="devcom-form-control"
-                                onTurnDirty={turnInputDirty}
-                            />
-                            <div className="devcom-form-error" >{formData.name.message}</div>
-                        </div>
-                        <div>
-                            <FormInput 
-                            {...formData.price}
-                                onChange={updateForm}
-                                className="devcom-form-control"
-                                onTurnDirty={turnInputDirty}
-                            />
-                            <div className="devcom-form-error" >{formData.price.message}</div>
-                        </div>
-                        <div >
-                            <FormSelect 
-                                className="devcom-form-control devcom-form-select-container"
-                                {...formData.categories}
-                                isMulti  
-                                onChange = { (obj : any) =>{
-                                                    const newFormData = forms.updateAndValidate(formData,"categories",obj);  
-                                                    console.log(newFormData.categories)
-                                                    setFormData(newFormData);  
-                                                    }}
-                                options = {categories} 
-                                onTurnDirty={turnInputDirty}
-                                styles={selectStyles}
-                                getOptionLabel={(obj : any) => obj.name }
-                                getOptionValue={(obj : any) => String(obj.id) }
-                                />
-                                <div className="devcom-form-error" >{formData.categories.message}</div>
-                        </div>
-                        <div>
-                            <FormTextArea 
-                            {...formData.description}
-                                onChange={updateForm}
-                                className="devcom-form-control devcom-textarea"
-                                onTurnDirty={turnInputDirty}
-                            />
-                            <div className="devcom-form-error" >{formData.description.message}</div>
-                        </div>
-                         {
-                            formData.imgUrl.value && 
-                            formData.imgUrl.value !== "" ? (
-                                <>
-                                    {/* <img src={URL.createObjectURL(formData.imgUrl.value)} 
+  return (
+    <>
+      <section className="devcom-new-product-form-section ">
+        <div className="devcom-container devcom-card devcom-container-column-center">
+          <h1>DADOS DO PRODUTO</h1>
+          <form className="devcom-container-column-center devcom-new-product-form" >
+            <div>
+              <FormInput
+                {...formData.name}
+                onChange={updateForm}
+                className="devcom-form-control"
+                onTurnDirty={turnInputDirty}
+              />
+              <div className="devcom-form-error">{formData.name.message}</div>
+            </div>
+            <div>
+              <FormInput
+                {...formData.price}
+                onChange={updateForm}
+                className="devcom-form-control"
+                onTurnDirty={turnInputDirty}
+              />
+              <div className="devcom-form-error">{formData.price.message}</div>
+            </div>
+            <div>
+              <FormSelect
+                className="devcom-form-control devcom-form-select-container"
+                {...formData.categories}
+                isMulti
+                onChange={(obj: any) => {
+                  const newFormData = forms.updateAndValidate(
+                    formData,
+                    "categories",
+                    obj
+                  );
+                  console.log(newFormData.categories);
+                  setFormData(newFormData);
+                }}
+                options={categories}
+                onTurnDirty={turnInputDirty}
+                styles={selectStyles}
+                getOptionLabel={(obj: any) => obj.name}
+                getOptionValue={(obj: any) => String(obj.id)}
+              />
+              <div className="devcom-form-error">
+                {formData.categories.message}
+              </div>
+            </div>
+            <div>
+              <FormTextArea
+                {...formData.description}
+                onChange={updateForm}
+                className="devcom-form-control devcom-textarea"
+                onTurnDirty={turnInputDirty}
+              />
+              <div className="devcom-form-error">
+                {formData.description.message}
+              </div>
+            </div>
+            {formData.imgUrl.value && formData.imgUrl.value !== "" ? (
+              <>
+                {/* <img src={URL.createObjectURL(formData.imgUrl.value)} 
                                         alt="not found" 
                                         width={"250px"}/>
                                     <button onClick={() => forms.update(formData,formData.imgUrl.name,null)}>Remove</button> */}
-                                    <FormInput 
-                                    {...formData.imgUrl}
-                                    onChange={imageSelected}
-                                />
-                                </>                               
-                            ) : 
-                            (
-                                <FormInput 
-                                    {...formData.imgUrl}
-                                    onChange={imageSelected}
-                                />
-                            )
-                         }
-
-                        
-
-                    </form>
-                    <div className="devcom-new-product-form-action-buttons">
-                        <ActionWhiteButton button={CancelButton} />
-                        <ActionBlueButton button={SaveButton} />
-                    </div>
-                </div>
-            </section>
-        </>
-    );
-
+                <FormInput {...formData.imgUrl} onChange={imageSelected} />
+              </>
+            ) : (
+              <FormInput {...formData.imgUrl} onChange={imageSelected} />
+            )}
+          </form>
+          <div className="devcom-new-product-form-action-buttons">
+            <ActionWhiteButton button={CancelButton} />
+            <ActionBlueButton button={SaveButton} />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -1,64 +1,95 @@
-
 ///Gera um novo Objeto do formulário onde o campo de nome "name" seja atualizado com o valor "newValue"
-export function update( inputs:any , name:string , newValue:any )
-{
-    return { ...inputs, [name]: {...inputs[name],value:newValue } };
+export function update(inputs: any, name: string, newValue: any) {
+  return { ...inputs, [name]: { ...inputs[name], value: newValue } };
 }
 
-
-export function updateAll( inputs : any , newValues : any)
-{
-    const data: any = {};
-    for(const name in inputs){
-
-        data[name] = { ...inputs[name], value : newValues[name]  };
-    }
-    return data;
+export function updateAll(inputs: any, newValues: any) {
+  const data: any = {};
+  for (const name in inputs) {
+    data[name] = { ...inputs[name], value: newValues[name] };
+  }
+  return data;
 }
 
 ///percorre a lista de inputs do form e cria um novo objeto
 /// com os nomes do input e seu valor atribuído
 /// Utilizado para "enxugar" a lista do form para apenas os valores necessários
 /// ainda mais se forem para ser utilizados no backend
-export function toValues(inputs:any)
-{
-    const data: any = {};
-    for(const name in inputs){
-
-        data[name] = inputs[name].value;
-    }
-    return data;
+export function toValues(inputs: any) {
+  const data: any = {};
+  for (const name in inputs) {
+    data[name] = inputs[name].value;
+  }
+  return data;
 }
 
 /// utiliza a função de validação do próprio campo ( se tiver )
 /// para verificar a veracidade dos dados contido no campo value
-/// e adiciona um novo campo ao input especifico dizendo se o valor 
+/// e adiciona um novo campo ao input especifico dizendo se o valor
 /// dele é INVÁLIDO
-export function validate(inputs : any , name : string)
-{
-    /// Verifica se campo do forms possui função de validação
-    if( !inputs[name].validation ) return inputs;
+export function validate(inputs: any, name: string) {
+  /// Verifica se campo do forms possui função de validação
+  if (!inputs[name].validation) return inputs;
 
-    const isInvalid = !inputs[name].validation( inputs[name].value );
+  const isInvalid = !inputs[name].validation(inputs[name].value);
 
-   return {...inputs, [name] : {...inputs[name], invalid : isInvalid.toString() } };
+  return {
+    ...inputs,
+    [name]: { ...inputs[name], invalid: isInvalid.toString() },
+  };
 }
 
-export function updateAndValidate(inputs:any , name:string , newValue:any)
-{
-    const updated = update(inputs,name, newValue);
-    return validate(updated,name);
+export function updateAndValidate(inputs: any, name: string, newValue: any) {
+  const updated = update(inputs, name, newValue);
+  return validate(updated, name);
 }
 
 //Gera um novo objeto de formulário contendo o campo "Dirty" (booleano) para o campo
 // do formulário que está sendo alterado.
-export function toDirty(inputs : any , name : string)
-{
-    return {...inputs, [name] : { ...inputs[name], dirty : "true" } }
+export function toDirty(inputs: any, name: string) {
+  return { ...inputs, [name]: { ...inputs[name], dirty: "true" } };
 }
 
-export function dirtyAndValidate(inputs : any , name : string)
+export function dirtyAndValidate(inputs: any, name: string) {
+  const dirty = toDirty(inputs, name);
+  return validate(dirty, name);
+}
+
+export function toDirtyAll(inputs: any) {
+  const newInputs: any = {};
+
+  for (var name in inputs) {
+    newInputs[name] = { ...inputs[name], dirty: "true" };
+  }
+  return newInputs;
+}
+
+export function validateAll(inputs: any) {
+  const newInputs: any = {};
+
+  for (var name in inputs) {
+    /// Verifica se campo do forms possui função de validação
+    if (!inputs[name].validation) {
+      newInputs[name] = { ...inputs[name] };
+    } else {
+      const isInvalid = !inputs[name].validation(inputs[name].value);
+      newInputs[name] = { ...inputs[name], invalid: isInvalid.toString() };
+    }
+  }
+  return newInputs;
+}
+
+export function dirtyAndValidateAll(inputs: any)
 {
-    const dirty = toDirty(inputs,name);
-    return validate(dirty,name);
+    return validateAll(toDirtyAll(inputs));    
+}
+
+export function hasAnyInvalid(inputs : any)
+{
+    for(var name in inputs)
+    {
+        if(inputs[name].dirty === "true" && inputs[name].invalid === "true" ) return true;
+    }
+
+    return false;
 }
