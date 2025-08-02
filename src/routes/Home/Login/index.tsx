603b09c9fb0e2c5a659c6ac1c2dc9bba4b210e56
Arthur Felipe Pallu <arthurfelipepallu@gmail.com>
2025-08-02 13:00:46 -0300

    Login form global message

--- src/routes/Home/Login/index.tsx
+++ src/routes/Home/Login/index.tsx
@@ -1,100 +1,131 @@
 import "./styles.css";
 import { useContext, useState } from "react";
-import * as authService from "../../../services/auth-service"
-import FormErrorMessage from "../../../Components/FormErrorMessage";
 import * as forms from "../../../utils/forms";
-import { ContextToken } from "../../../utils/context-API";
 import { useNavigate } from "react-router-dom";
 import { history } from "../../../utils/history";
 import FormInput from "../../../Components/FormInput";
+import { ContextToken } from "../../../utils/context-API";
+import * as authService from "../../../services/auth-service";
+import FormErrorMessage from "../../../Components/FormErrorMessage";
 
 export default function Login() {
-
   const navigate = useNavigate();
 
-  const {setContextTokenPayload} = useContext(ContextToken);
+  const [errorOcurred, setOcurredError] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>(
+    "Usuário ou senha inválida"
+  );
+
+  const { setContextTokenPayload } = useContext(ContextToken);
 
   const [loginInfo, setloginInfo] = useState<any>({
-                                              username:{
-                                                value: "",
-                                                id:"username",
-                                                name:"username",
-                                                type:"text",
-                                                placeholder:"Email",
-                                                validation: function(value:string){
-                                                  return /^[a-zA-Z0-9.!#$%&*+=?^_´{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value.toLowerCase());
-                                                },
-                                                message:"Favor informar Email válido"
-                                              },
-                                              password:{
-                                                value: "",
-                                                id:"password",
-                                                name:"password",
-                                                type:"password",
-                                                placeholder:"Senha",
-                                                message:"Favor informar Senha válida"
-                                              }
-                                            });
+    username: {
+      value: "",
+      id: "username",
+      name: "username",
+      type: "text",
+      placeholder: "Email",
+      validation: function (value: string) {
+        return /^[a-zA-Z0-9.!#$%&*+=?^_´{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+          value.toLowerCase()
+        );
+      },
+      message: "Favor informar Email válido",
+    },
+    password: {
+      value: "",
+      id: "password",
+      name: "password",
+      type: "password",
+      placeholder: "Senha",
+      message: "Favor informar Senha válida",
+    },
+  });
 
-  const [authenticationError,setAuthenticationError] = useState<boolean>(false);
+  const [authenticationError, setAuthenticationError] =
+    useState<boolean>(false);
 
-  function handleSubmit(event:any){
+  function handleSubmit(event: any) {
     event.preventDefault();
-    authService.loginRequest(forms.toValues(loginInfo))
-     .then(response =>{
-            console.log(response.data);
-            authService.saveAccessToken(response.data.access_token); 
-            setContextTokenPayload(authService.getAccessTokenPayload());
-            history.push("/admin");
-    })
 
-    setAuthenticationError(!authService.isAuthenticated())
+    setOcurredError(false);
+
+    const loginValidated = forms.dirtyAndValidateAll(loginInfo);
+
+    if (forms.hasAnyInvalid(loginValidated)) {
+      setloginInfo(loginValidated);
+      return;
+    }
+
+    authService
+      .loginRequest(forms.toValues(loginInfo))
+      .then((response) => {
+        console.log(response.data);
+        authService.saveAccessToken(response.data.access_token);
+        setContextTokenPayload(authService.getAccessTokenPayload());
+        history.push("/admin");
+      })
+      .catch((error) => {
+        setOcurredError(true);
+        console.log(error);
+        setErrorMessage(error.response.data.error_description);
+      });
+
+    setAuthenticationError(!authService.isAuthenticated());
     setAuthenticationError(authenticationError);
   }
 
   function handleLoginCheck() {}
 
-    function updateFormInput(event : any)
-    {
-        //forms atualizado e validado
-        const result = forms.updateAndValidate(loginInfo,event.target.name,event.target.value);
-        //finalmente forms setado para valores novos
-        setloginInfo(result);
-    }
-    function turnInputDirty(name : string)
-    {
-        setloginInfo(forms.dirtyAndValidate(loginInfo,name));
-    }
-
+  function updateFormInput(event: any) {
+    //forms atualizado e validado
+    const result = forms.updateAndValidate(
+      loginInfo,
+      event.target.name,
+      event.target.value
+    );
+    //finalmente forms setado para valores novos
+    setloginInfo(result);
+  }
+  function turnInputDirty(name: string) {
+    setloginInfo(forms.dirtyAndValidate(loginInfo, name));
+  }
 
   return (
     <>
       <main>
         <section id="login-section">
           <div className="login-form-container">
-            <form onSubmit={handleSubmit} >
+            <form onSubmit={handleSubmit}>
               <div className="login-form-input-conatiner">
                 <div>
-                    <FormInput 
-                        {...loginInfo.username}
-                        onChange={updateFormInput}
-                        className="devcom-form-control"
-                        onTurnDirty={turnInputDirty}
-                    />
-                    <div className="devcom-form-error" >{loginInfo.username.message}</div>
+                  <FormInput
+                    {...loginInfo.username}
+                    onChange={updateFormInput}
+                    className="devcom-form-control"
+                    onTurnDirty={turnInputDirty}
+                  />
+                  <div className="devcom-form-error">
+                    {loginInfo.username.message}
+                  </div>
                 </div>
               </div>
               <div className="login-form-input-conatiner">
                 <div>
-                    <FormInput 
-                        {...loginInfo.password }
-                        onChange={updateFormInput}
-                        className="devcom-form-control"
-                        onTurnDirty={turnInputDirty}
-                    />
-                    <div className="devcom-form-error" >{loginInfo.password.message}</div>
+                  <FormInput
+                    {...loginInfo.password}
+                    onChange={updateFormInput}
+                    className="devcom-form-control"
+                    onTurnDirty={turnInputDirty}
+                  />
+                  <div className="devcom-form-error">
+                    {loginInfo.password.message}
+                  </div>
                 </div>
               </div>
+              {errorOcurred && (
+                <div className="devcom-form-global-error">{errorMessage}</div>
+              )}
               <button
                 type="submit"
                 onClick={handleLoginCheck}
